23d81fc070ce1db6b2078785bb95e30f3f68f595
Diego Leiva <di3go21@gmail.com>
2020-04-09 18:43:00 +0200

    segundo comit

--- GitEDDjava/src/Movimiento.java
+++ GitEDDjava/src/Movimiento.java
@@ -1,4 +1,42 @@
+public class Movimiento { //clase movimiento
+	
+	private int codigo;
+	private char tipoOperacion;
+	private int cantidad;
+	
+	public Movimiento(int codigo, char tipoOperacion, int cantidad) {
+		this.codigo = codigo;
+		this.tipoOperacion = tipoOperacion;
+		this.cantidad = cantidad;
+	}
 
-public class Movimiento {
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public char getTipoOperacion() {
+		return tipoOperacion;
+	}
+
+	public void setTipoOperacion(char tipoOperacion) {
+		this.tipoOperacion = tipoOperacion;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	@Override
+	public String toString() {
+		return "Movimiento [codigo=" + codigo + ", tipoOperacion=" + tipoOperacion + ", cantidad=" + cantidad + "]";
+	}
 
 }
--- GitEDDjava/src/Producto.java
+++ GitEDDjava/src/Producto.java
@@ -1,4 +1,69 @@
 
-public class Producto {
+public class Producto { //clase producto
+	
+	private String nombre;
+	private int codigo;
+	private int cantidad;
+	private double precio;
+
+	
+	public Producto( int codigo, String nombre,int cantidad, double precio) {
+		this.nombre = nombre;
+		this.codigo = codigo;
+		this.cantidad= cantidad;
+		this.precio = precio;
+		
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [nombre=" + nombre + ", codigo=" + codigo + ", cantidad=" + cantidad + ", precio=" + precio
+				+ "]";
+	}
+	
+	public String cadenaparaInventario() { //devuelve el producto en una cadena lista para escribirla en el fichero
+		return (codigo+" "+nombre+" "+cantidad+" "+precio);
+	}
+
+
+	
+	
+	
+	
+	
 
 }
+
